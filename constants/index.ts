89ccf5258ed1e73cb431ea56e693470a73966b9c
Bensolve <54053468+Benjamin569@users.noreply.github.com>
2024-12-15 19:17:48 +0000

    header home

--- constants/index.ts
+++ constants/index.ts
@@ -21,9 +21,4 @@ export const sidebarLinks = [
     },
   ];
 
-  declare interface HeaderBoxProps {
-    type?: "title" | "greeting";
-    title: string;
-    subtext: string;
-    user?: string;
-  }
+ 
